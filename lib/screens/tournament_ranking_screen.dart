0091075e1592dcd4daf7b8b933b339f240581c83
Mário Santos <marioh90@gmail.com>
2025-06-27 17:55:51 -0300

    refactor: remove unused imports and update app version
    Clean up unused imports from screens to improve code maintainability and update app version to 1.0.3+1

--- lib/screens/tournament_ranking_screen.dart
+++ lib/screens/tournament_ranking_screen.dart
@@ -7,8 +7,6 @@ import 'package:placar_iterativo_app/models/team.dart';
 import 'package:placar_iterativo_app/providers/teams_provider.dart';
 import 'package:placar_iterativo_app/services/tts_service.dart';
 import 'package:placar_iterativo_app/services/audio_service.dart';
-import 'package:placar_iterativo_app/widgets/animated_widgets.dart';
-import 'package:placar_iterativo_app/utils/responsive_utils.dart';
 
 class TournamentRankingScreen extends StatefulWidget {
   final Tournament tournament;
